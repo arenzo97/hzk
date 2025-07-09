4f7ca49246fdbbc3e3b00da88b4c29c010366e8a
Luis <larenas97@gmail.com>
2025-07-09 01:14:33 +0100

    fix: styles loading at least

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,15 +1,14 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
-
-// https://astro.build/config
+import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   output: 'server',
   adapter: node({
-    mode: 'standalone' // or 'middleware'
+    mode: 'standalone'
   }),
   server: {
     host: true,
     port: 4321,
   },
+  integrations: [tailwind()],
 });
---
+++ frontend/tailwind.config.mjs
@@ -0,0 +1,12 @@
+import typography from '@tailwindcss/typography';
+
+export default {
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
